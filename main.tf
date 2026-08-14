7c1c1cbb2d9bcbe2dd6a4b335bcb59733b1273d4
Akhil Jain <meakhiljain1191@gmail.com>
2026-08-14 11:54:35 +0530

    added stg code

--- main.tf
+++ main.tf
@@ -10,3 +10,11 @@ terraform {
 provider "azurerm" {
 features {}
 }
+
+resource "azurerm_storage_account" "stg" {
+  name                     = "storageaccountname"
+  resource_group_name      = "rg-1"
+  location                 = "centralindia"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
